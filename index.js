165da4bb879255a1fc10324fca061d7714c2346f
theNubiadev <ajaiyeobajibola@gmail.com>
2025-01-30 15:31:35 +0100

    corrected the github url format

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
         res.json({
             email: "ajaiyeobajibola@gmail.com",
             current_datetime: new Date().toISOString(),
-            github_url: "https://github.com/theNubiadev/back0.git"
+            github_url: "https://github.com/theNubiadev/back0"
         });
     });
 
